c5c47dfa3d254722a8f46fac8592a65bcf537cf8
LucasCostakt <lucascosta.kt@gmail.com>
2020-10-23 18:15:32 -0300

    Add bootstrap

---
+++ bootstrap/bootapp.go
@@ -0,0 +1,14 @@
+package bootstrap
+
+import (
+	"net/http"
+
+	"github.com/MVC/controllers"
+)
+
+func BootAplication() {
+	http.HandleFunc("/employees", controllers.GetEmployee)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
