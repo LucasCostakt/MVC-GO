63f762e09850428dfac453666220d29b13f84f56
LucasCostakt <lucascosta.kt@gmail.com>
2020-10-25 20:53:05 -0300

    Refatorando
    Refatorando nomes de pasta e adicionando pdf de explicação do projeto

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/MVC/bootstrap"
+	"github.com/MVC/view"
 )
 
 func main() {
-	bootstrap.BootAplication()
+	view.BootAplication()
 }
--- view/bootapp.go
+++ view/bootapp.go
@@ -1,4 +1,4 @@
-package bootstrap
+package view
 
 import (
 	"net/http"
